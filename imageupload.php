bf9a937c3a3fb58aef3a0f835d82a3b92f087a81
Onyemenam Ndubuisi <onyemenamndu@gmail.com>
2018-08-23 13:43:05 +0100

    increase image size limit to 5mb

--- imageupload.php
+++ imageupload.php
@@ -24,7 +24,7 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["fileToUpload"]["size"] > 5000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
